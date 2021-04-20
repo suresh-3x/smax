4f945f81618f208109eeabb889219d86b8013b41
suresh-3x <77671068+suresh-3x@users.noreply.github.com>
2021-04-20 13:00:42 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='smax',
     version='0.0.1',
     description='My private package from private github repo',
-    url='githttps://github.com/suresh-3x/smax.git',
+    url='https://github.com/suresh-3x/smax.git',
     author='suresh-3x',
     author_email='suresh.37x@gmail.com',
     license='unlicense',
