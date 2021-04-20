b1c3cb533a23c4c76820b55fee49001e617e4857
suresh-3x <suresh.37x@gmail.com>
2021-04-20 12:27:33 +0530

    init commit 2

--- setup.py
+++ setup.py
@@ -1,17 +1,12 @@
 from setuptools import setup, find_packages
 
-with open('README.md') as readme_file:
-    README = readme_file.read()
-
-with open('HISTORY.md') as history_file:
-    HISTORY = history_file.read()
 
 setup_args = dict(
     name='smax',
     version='0.0.1',
     description='Useful tools to work with Elastic stack in Python',
     long_description_content_type="text/markdown",
-    long_description=README + '\n\n' + HISTORY,
+    long_description="Nothing",
     license='MIT',
     packages=find_packages(),
     author='Suresh-3x',
