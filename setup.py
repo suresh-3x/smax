ba2410d4d74d9938e2d9b697e5887d41e4fdaf2b
suresh-3x <suresh.37x@gmail.com>
2021-04-20 12:30:10 +0530

    init commit 2

--- setup.py
+++ setup.py
@@ -1,24 +1,13 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-
-setup_args = dict(
+setup(
     name='smax',
     version='0.0.1',
-    description='Useful tools to work with Elastic stack in Python',
-    long_description_content_type="text/markdown",
-    long_description="Nothing",
-    license='MIT',
-    packages=find_packages(),
-    author='Suresh-3x',
+    description='My private package from private github repo',
+    url='githttps://github.com/suresh-3x/smax.git',
+    author='suresh-3x',
     author_email='suresh.37x@gmail.com',
-    keywords=['Elastic', 'ElasticSearch', 'ElasticStack'],
-    url='https://github.com/suresh-3x/smax',
-    download_url='https://github.com/suresh-3x/smax'
+    license='unlicense',
+    packages=['ptolemaios'],
+    zip_safe=False
 )
-
-install_requires = [
-   
-]
-
-if __name__ == '__main__':
-    setup(**setup_args, install_requires=install_requires)
